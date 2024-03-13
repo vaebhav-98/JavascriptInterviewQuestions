fd20e9895520e09546ad0d80146f962d77a8533f
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-03-13 23:52:35 +0530

    changes

--- Question1.js
+++ Question1.js
@@ -2,8 +2,13 @@
 
 let arr = ['hello', 'world', 'java', 'hello', 'java'];
 
-const result = arr.reduce((item, newitem, arra)=>{
-    return newitem
-})
+// const result = arr.reduce((item, newitem, arra)=>{
+//     return newitem
+// })
+
+const result = arr.reduce((acc, item)=>{
+    acc[item] = (acc[item] || 0) + 1;
+    return acc;
+}, {})
 
 console.log(result, 'resutl')

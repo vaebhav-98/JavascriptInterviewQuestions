da509eedfc26bc0edbd0f5d3e4bade9d2fd327e5
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-05-01 23:31:21 +0530

    k largest element in array

--- KLargestElement.js
+++ KLargestElement.js
@@ -20,3 +20,14 @@ Constraints:
 1 <= k <= nums.length <= 105
 - 104 <= nums[i] <= 104 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+let findKthLargest = (nums, k) => {
+    nums.sort((a, b) => b - a)
+    return nums[k - 1];
+};
+
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
